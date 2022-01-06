29795dbbc902595b89146bb49136781886d94c13
Dani Meier <me@danimeier.dev>
2022-01-06 22:18:37 +0100

    Configure logging

--- src/Flyingdot.Elgato.Keylight/ElgatoApiClient.cs
+++ src/Flyingdot.Elgato.Keylight/ElgatoApiClient.cs
@@ -1,26 +1,37 @@
+using System;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Flyingdot.Elgato.Keylight.Model;
+using Microsoft.Extensions.Logging;
 
 namespace Flyingdot.Elgato.Keylight
 {
     public class ElgatoApiClient
     {
         private readonly HttpClient _httpClient;
+        private readonly ILogger<ElgatoApiClient> _logger;
 
-        public ElgatoApiClient(HttpClient httpClient)
+        public ElgatoApiClient(HttpClient httpClient, ILogger<ElgatoApiClient> logger)
         {
             _httpClient = httpClient;
+            _logger = logger;
         }
 
         public async Task Put(ElgatoRequest request)
         {
-            string json = JsonSerializer.Serialize<ElgatoRequest>(request);
-            HttpResponseMessage responseMessage = await _httpClient.PutAsync(
-                $"{_httpClient.BaseAddress}elgato/lights",
-                new StringContent(json));
-            responseMessage.EnsureSuccessStatusCode();
+            try
+            {
+                string json = JsonSerializer.Serialize(request);
+                HttpResponseMessage responseMessage = await _httpClient.PutAsync(
+                    $"{_httpClient.BaseAddress}elgato/lights",
+                    new StringContent(json));
+                responseMessage.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException e)
+            {
+                _logger.LogError("Elgato API Request failed: {ErrorMessage}", e.Message);
+            }
         }
     }
 }
